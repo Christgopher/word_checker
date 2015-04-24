c02497d7bfe7a56ad1baf02652af6c8d85d35a1f
Guest User <student@epicodus.com>
2015-04-24 09:38:38 -0700

    it will now counts all words regardless of basic punction

--- lib/word_checker.rb
+++ lib/word_checker.rb
@@ -1,8 +1,30 @@
 class String
   define_method(:word_checker) do |user_word|
+
     word_counter = 0
-    if self.==(user_word)
-      word_counter = word_counter.+(1)
+    user_sentence_postpunc_letters = []
+    user_sentence_letters = self.split("")
+
+    user_sentence_letters.each() do |letter|
+      if letter.!=(",")
+        if letter.!=(":")
+          if letter.!=("!")
+            if letter.!=(".")
+              if letter.!=("?")
+                user_sentence_postpunc_letters.push(letter)
+              end
+            end
+          end
+        end
+      end
     end
+    user_sentence = user_sentence_postpunc_letters.join()
+    user_sentence_words = user_sentence.split(" ")
+    user_sentence_words.each() do |word|
+      if word.==(user_word)
+      word_counter = word_counter.+(1)
+      end
+    end
+    word_counter
   end
 end
--- spec/word_checker_spec.rb
+++ spec/word_checker_spec.rb
@@ -5,4 +5,12 @@ describe('String#wwrd_checker') do
   it('checks if a string appears in itself') do
     expect(("monkey").word_checker("monkey")).to(eq(1))
   end
+
+  it('counts instances of word in sentence') do
+    expect(("monkey is a money making monkey").word_checker("monkey")).to(eq(2))
+  end
+
+  it('deals with commas and other puncation') do
+    expect(("monkey is a money, making, monkey: monkey!").word_checker("monkey")).to(eq(3))
+  end
 end
