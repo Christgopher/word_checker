f68db1ed075750a1b855a8395e75429e5263c1f2
Guest User <student@epicodus.com>
2015-04-24 10:14:41 -0700

    edited for clarity

--- spec/word_checker_spec.rb
+++ spec/word_checker_spec.rb
@@ -1,8 +1,8 @@
 require('rspec')
 require('word_checker')
 
-describe('String#wwrd_checker') do
-  it('checks if a string appears in itself') do
+describe('String#word_checker') do
+  it('checks if a string counts itself as an instance') do
     expect(("monkey").word_checker("monkey")).to(eq(1))
   end
 
@@ -10,7 +10,7 @@ describe('String#wwrd_checker') do
     expect(("monkey is a money making monkey").word_checker("monkey")).to(eq(2))
   end
 
-  it('deals with commas and other puncation') do
+  it('deals with commas and other punctuation') do
     expect(("monkey is a money, making, monkey: monkey!").word_checker("monkey")).to(eq(3))
   end
 end
